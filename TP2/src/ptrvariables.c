eddff3c47b14e741ea5e046a3f3e9b8715f042f9
Malika Amrani <157502314+malika95290@users.noreply.github.com>
2025-12-02 10:38:14 +0100

    Refactor ptrvariables.c with new variable values

--- TP2/src/ptrvariables.c
+++ TP2/src/ptrvariables.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
-#include <stdint.h>
 
 int main() {
-    char c = 0x12;
-    short s = 0x1234;
-    int i = 0x12345678;
-    long int li = 0x11223344;
-    long long int lli = 0x1122334455667788;
-    float f = 1.0f;
-    double d = 2.0;
-    long double ld = 3.0;
+    char c = 10;
+    short s = 200;
+    int i = 3000;
+    long int li = 40000;
+    long long int lli = 500000;
+    float f = 1.5f;
+    double d = 2.5;
+    long double ld = 3.5;
 
     char *pc = &c;
     short *ps = &s;
@@ -21,42 +20,33 @@ int main() {
     long double *pld = &ld;
 
     printf("Avant manipulation :\n");
-    printf("c  : adresse=%p, valeur=%02x\n", (void*)pc, (unsigned char)c);
-    printf("s  : adresse=%p, valeur=%04x\n", (void*)ps, (unsigned short)s);
-    printf("i  : adresse=%p, valeur=%08x\n", (void*)pi, (unsigned int)i);
-    printf("li : adresse=%p, valeur=%08lx\n", (void*)pli, (unsigned long)li);
-    printf("lli: adresse=%p, valeur=%016llx\n", (void*)plli, (unsigned long long)lli);
+    printf("c  : adresse=%p, valeur=%d\n", (void*)pc, c);
+    printf("s  : adresse=%p, valeur=%d\n", (void*)ps, s);
+    printf("i  : adresse=%p, valeur=%d\n", (void*)pi, i);
+    printf("li : adresse=%p, valeur=%ld\n", (void*)pli, li);
+    printf("lli: adresse=%p, valeur=%lld\n", (void*)plli, lli);
+    printf("f  : adresse=%p, valeur=%f\n", (void*)pf, f);
+    printf("d  : adresse=%p, valeur=%f\n", (void*)pd, d);
+    printf("ld : adresse=%p, valeur=%Lf\n", (void*)pld, ld);
 
-    printf("f  : adresse=%p, valeur=%08x\n", (void*)pf, *(uint32_t*)&f);
-    printf("d  : adresse=%p, valeur=%016llx\n", (void*)pd, *(uint64_t*)&d);
-
-    unsigned char *ldbytes = (unsigned char*)&ld;
-    printf("ld : adresse=%p, valeur=", (void*)pld);
-    for (int k = sizeof(long double)-1; k >= 0; k--) printf("%02x", ldbytes[k]);
-    printf("\n");
-
-    *pc = 0x34;
-    *ps = 0x5678;
-    *pi = 0x87654321;
-    *pli = 0xdeadbeef;
-    *plli = 0xaabbccddeeff0011;
-    *pf = 2.0f;
-    *pd = 4.0;
-    *pld = 5.0;
+    *pc = 20;
+    *ps = 300;
+    *pi = 6000;
+    *pli = 80000;
+    *plli = 900000;
+    *pf = 3.0f;
+    *pd = 5.0;
+    *pld = 7.0;
 
     printf("\nAprès manipulation :\n");
-    printf("c  : adresse=%p, valeur=%02x\n", (void*)pc, (unsigned char)c);
-    printf("s  : adresse=%p, valeur=%04x\n", (void*)ps, (unsigned short)s);
-    printf("i  : adresse=%p, valeur=%08x\n", (void*)pi, (unsigned int)i);
-    printf("li : adresse=%p, valeur=%08lx\n", (void*)pli, (unsigned long)li);
-    printf("lli: adresse=%p, valeur=%016llx\n", (void*)plli, (unsigned long long)lli);
-
-    printf("f  : adresse=%p, valeur=%08x\n", (void*)pf, *(uint32_t*)&f);
-    printf("d  : adresse=%p, valeur=%016llx\n", (void*)pd, *(uint64_t*)&d);
-
-    printf("ld : adresse=%p, valeur=", (void*)pld);
-    for (int k = sizeof(long double)-1; k >= 0; k--) printf("%02x", ldbytes[k]);
-    printf("\n");
+    printf("c  : adresse=%p, valeur=%d\n", (void*)pc, c);
+    printf("s  : adresse=%p, valeur=%d\n", (void*)ps, s);
+    printf("i  : adresse=%p, valeur=%d\n", (void*)pi, i);
+    printf("li : adresse=%p, valeur=%ld\n", (void*)pli, li);
+    printf("lli: adresse=%p, valeur=%lld\n", (void*)plli, lli);
+    printf("f  : adresse=%p, valeur=%f\n", (void*)pf, f);
+    printf("d  : adresse=%p, valeur=%f\n", (void*)pd, d);
+    printf("ld : adresse=%p, valeur=%Lf\n", (void*)pld, ld);
 
     return 0;
 }
